926359862de44d1012c2735d682893158430614e
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-07-29 11:18:48 +0530

    sarbo codes added

--- backend/app.js
+++
@@ -1,9 +0,0 @@
-/*$$$To Be Done$$$
-- Create Auth Workflow either by using Third Party OAuth / Creating our own auth server using
- passport.js and bcrypt.
-- Creating API's for Saving images and messages in JSON format in Cloud (aws/azure/do) and retrive it to render in React Frontend
-OR
-handling it in an easier way by using Mongo to store personal data:TBD
-- Deployment with domain name.
-
-*/
