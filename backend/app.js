d2adb2d06c25eb37e6a4fd6b431409301016ad17
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-08-17 15:01:21 +0530

    node-server init

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,13 @@
-/*$$$To Be Done$$$
-- Create Auth Workflow either by using Third Party OAuth / Creating our own auth server using
- passport.js and bcrypt.
-- Creating API's for Saving images and messages in JSON format in Cloud (aws/azure/do) and retrive it to render in React Frontend
-OR
-handling it in an easier way by using Mongo to store personal data:TBD
-- Deployment with domain name.
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser"); 
+const PORT = 6969;
 
-*/
+app.get('/',(req,res)=>{
+    res.send("Works");
+})
+
+app.listen(6969,()=>{
+    console.log(`Server is running on port ${PORT}`);
+})
