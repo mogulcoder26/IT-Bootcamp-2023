85c31cb4c382953648566c4c1066b9061f9a6be2
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-08-17 15:26:18 +0530

    aws codes init

---
+++ backend/middlewares/@aws/s3config.js
@@ -0,0 +1,18 @@
+const s3= require('aws-sdk/clients/s3');
+const fs= `require('fs')`;
+require('dotenv').config();
+
+const bucketName= process.env.AWS_BUCKET_NAME;
+const region= process.env.AWS_BUCKET_REGION;
+const accessKeyId= process.env.AWS_ACCESS_KEY_ID;
+const secretAccessKey= process.env.AWS_SECRET_ACCESS_KEY;
+
+console.log('bucket name = ' + bucketName);
+
+const s3= new s3({
+    region,
+    accessKeyId,
+    secretAccessKey
+});
+
+export default s3;
