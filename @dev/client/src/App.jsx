56b796531757d08f18be4c39966ea8031411d5e0
Punit Kumar <tagpunitkumar@gmail.com>
2023-08-29 10:08:51 +0530

    commit

--- @dev/client/src/App.jsx
+++ @dev/client/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
   return (
     <>
-    <div className="bg-gradient-to-br from-slate-900 via-purple-900 to-slate-900 h-screen">
+    <div className="bg-slate-900 bg-[radial-gradient(at_82%_22%,rgba(3,169,244,0.5)_0,transparent_22%),radial-gradient(at_2%_78%,rgba(3,169,244,0.5),transparent_22%)] bg-repeat-y h-screen">
       <Navbar />
       <Hero />
       <Footer />
