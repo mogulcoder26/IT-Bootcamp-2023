4992c0e4f2e0ca8110747ec1dbb67d09b492de05
Punit Kumar <tagpunitkumar@gmail.com>
2023-09-01 22:25:50 +0530

    final

--- @dev/client/src/components/faviconCR.jsx
+++ @dev/client/src/components/faviconCR.jsx
@@ -3,24 +3,22 @@ import { motion } from "framer-motion"
 export default function FaviconCR() {
     return (
         <>
-        <div className="flex flex-col gap-4">
-            <div className="relative w-[200px] h-[200px] rounded-[200px] overflow-hidden">
-            <motion.div
-                className="absolute inset-0 bg-gradient-to-tr from-green-300 via-blue-500 to-purple-600"
-                style={{ rotate: '0deg' }} // Initial rotation angle
-                animate={{ rotate: '360deg' }} // Rotate 360 degrees
-                transition={{ duration: 0.9, repeat: Infinity, ease:"linear" }}
-            />
-            <div className="flex justify-center items-center">
-                <img
-                src="/tars_iiit_bh_logo 1.png"
-                alt="Logo"
-                className="w-[180px] h-[180px] rounded-[180px]"
+        <motion.div whileHover={{scale: 1.1, cursor: "pointer"}} className="flex flex-col gap-4">
+            <div className="relative w-[200px] h-[200px] rounded-lg overflow-hidden">
+                <motion.div
+                    className="absolute -inset-10 bg-gradient-to-tr from-green-300 via-blue-500 to-purple-600"
+                    style={{ rotate: '0deg' }}
+                    animate={{ rotate: '360deg' }}
+                    transition={{ duration: 0.9, repeat: Infinity, ease:"linear" }}
                 />
+                <div className="flex justify-center h-full w-full">
+                    <div className="flex flex-col rounded-lg items-center font-bold text-2xl justify-center text-white bg-black h-[99] w-full z-50 m-2">
+                    Batch 2023
+                    <div className="font-medium text-2xl">The freshers</div>
+                    </div>
+                </div>
             </div>
-            </div>
-            <h1 className="text-white font-semibold max-w-[200px]">Lorem ipsum dolor, sit amet consectetur adipisicing elit.</h1>
-        </div>
+        </motion.div>
         </>
     )
 }
