be1af8a079a1f441148f7f997926a24474e2d2b3
Soubhik Kumar Gon <b422056@iiit-bh.ac.in>
2023-09-20 00:54:45 +0530

    integrated client code

--- nextclient/app/layout.tsx
+++ nextclient/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import Appbar from "../components/Appbar";
 import Providers from "../components/Providers";
 import { Inter } from "next/font/google";
@@ -14,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <Providers>
-          <Appbar />
+          <Navbar />
           {children}
         </Providers>
       </body>
---
+++ nextclient/app/pk/images.jsx
@@ -0,0 +1,9 @@
+
+const image1 = "https://odiapreneur.com/img/20220228_093513.jpg"
+const image2 = "https://img.collegepravesh.com/2016/05/IIIT-Bhubaneswar.jpg"
+const image3 = "https://images.shiksha.com/mediadata/images/1503643027phpehvmXY.jpeg"
+const image4 = "https://images.collegedunia.com/public/college_data/images/campusimage/143539476514099985941016548_474343542660552_2141979448_n.jpg"
+
+const images = [image1, image2, image3, image4]
+
+export default images
