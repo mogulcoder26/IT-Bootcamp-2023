99571be3237490682f89d53ab2d0a3363570319d
Punit Kumar <tagpunitkumar@gmail.com>
2023-09-21 23:59:15 +0530

    something

---
+++ @dev/client/src/Pages/Registration/index.jsx
@@ -0,0 +1,14 @@
+export default  function Registration() {
+  return (
+    <>
+        <div className="flex justify-around h-screen m-4 gap-4">
+            <div className="flex bg-gray-400 w-full justify-center items-center">
+                HEllo
+            </div>
+            <div className="w-full bg-gray-800 rounded-lg">
+                HElloo
+            </div>
+        </div>
+    </>
+  )
+}
