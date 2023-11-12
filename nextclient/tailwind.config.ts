478163f6232c60f1df9efa057f97eabaf6b4783b
Punit Kumar <tagpunitkumar@gmail.com>
2023-11-12 12:51:30 +0530

    sarbo fetish

--- nextclient/tailwind.config.ts
+++ nextclient/tailwind.config.ts
@@ -2,9 +2,11 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite-react/**/*.js",
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
@@ -15,6 +17,5 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
 }
 export default config
